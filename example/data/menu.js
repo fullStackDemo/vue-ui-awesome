d529cbafb283b49144ecd60e3bbd354c6563686e
wangzhao <wangzhao@go-goal.com>
2019-03-07 15:34:00 +0800

    initializing less module

---
+++ example/data/menu.js
@@ -0,0 +1,10 @@
+import Hello from '../../src/components/Hello.vue';
+
+const data = [
+  {
+    name: 'Hello',
+    component: Hello
+  },
+];
+
+export default data;
--- example/index.vue
+++ example/index.vue
@@ -1,17 +1,52 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-
-  </div>
+	<div id="app">
+		<div :style="{height: boxHeight + 'px'}" class="box">
+			<div class="boxLeft">
+				<div class="logo">
+					<img alt="Vue logo" src="./assets/logo.png">
+				</div>
+				<h3>Vue component</h3>
+        <div class="navBox">
+          <div class="navItem" v-for="(item, index) in menuData" :key="index">
+            {{item.name}}
+          </div>
+        </div>
+			</div>
+			<div class="boxRight">
+        <div :is="currentComponent"></div>
+      </div>
+		</div>
+	</div>
 </template>
 
 <script>
-
+import menuData from './data/menu.js';
 export default {
-  name: 'app',
-}
+	name: 'app',
+	data() {
+    const currentComponent = menuData[0].component;
+		return {
+      boxHeight: 0,
+      menuData,
+      currentComponent
+		};
+	},
+	mounted() {
+		this.calcHeight();
+
+		//window resize
+		window.addEventListener('resize', () => {
+			this.calcHeight();
+		});
+	},
+	methods: {
+		calcHeight() {
+			this.boxHeight = window.innerHeight;
+		},
+	},
+};
 </script>
 
 <style lang="less">
-@import "./less/index.less";
+@import './less/index.less';
 </style>
